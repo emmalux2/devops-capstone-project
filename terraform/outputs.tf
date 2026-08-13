b5fcad2b7b9b0062bba65cc00b94bb5e9f39d8dc
emmanuel <emmalux2@gmail.com>
2026-08-13 21:20:37 +0100

    Fix outputs to match devops_instance

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,14 @@
-# Show the public IP of the EC2 instance
 output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.devops_instance.public_ip
 }
 
-# Show the public DNS name of the EC2 instance
 output "instance_public_dns" {
-  description = "Public DNS name of the EC2 instance"
-  value       = aws_instance.app_server.public_dns
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.devops_instance.public_dns
 }
 
-# Show the subnet ID used
 output "subnet_id" {
   description = "Subnet ID where the EC2 instance is launched"
-  value       = aws_instance.app_server.subnet_id
-}
-
-# Show the security group ID used
-output "security_group_id" {
-  description = "Security Group ID attached to the EC2 instance"
-  value       = aws_security_group.devops_sg.id
+  value       = aws_instance.devops_instance.subnet_id
 }
